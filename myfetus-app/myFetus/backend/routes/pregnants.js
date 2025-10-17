cc69ab647dbd3a25df7cbf689556be446e4eaae3
Lucasrc22 <lucasrios2210@gmail.com>
2025-10-17 15:32:11 -0300

    expo rodando localmente. E o banco de dados e backend em Docker

--- myfetus-app/myFetus/backend/routes/pregnants.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pregnantController = require('../controllers/pregnantController');
-
-router.post('/', pregnantController.createPregnant);
-router.get('/', pregnantController.getPregnants);
-router.put('/:id', pregnantController.updatePregnant)
-
-module.exports = router;
