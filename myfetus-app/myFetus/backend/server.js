8a2e0edc06553d3cf90cfe1cc4daa3cfaa4d9aec
Lucasrc22 <lucasrios2210@gmail.com>
2025-10-16 21:42:02 -0300

    Atualizado conteúdo da branch Lucas com base na feature/Elayne-MyFetus

--- myfetus-app/myFetus/backend/server.js
+++ myfetus-app/myFetus/backend/server.js
@@ -28,10 +28,11 @@ app.listen(3000, () => {
     console.log('servidor rodando na porta 3000')
 })
 
-
+/* pra depois:
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+*/
